8eba58e73e65c27fd9d27d9a0a2feb96341e3ede
CodekulNitin7872 <codekul.nitin@gmail.com>
2023-01-17 19:11:07 +0530

    medication error api

--- src/App.js
+++ src/App.js
@@ -8,13 +8,10 @@ import About from "./components/ReactRouter/About/About";
 import Navbar from "./components/ReactRouter/Home/Navbar";
 import HomePage from './components/ReactRouter/Home/HomePage'
 import Contact from './components/ReactRouter/ContactUs/Contact'
-
 import Products from "./components/ReactRouter/UseParamas/Products";
 import ProfilePage from "./components/ReactRouter/UseParamas/ProfilePage";
 
-
-
-
+// https://www.freecodecamp.org/news/avoid-prop-drilling-with-react-context-api/
 function App() {
   return (
     <div className="App">
